5d4cf2b76340ad2efcf10af2f26ada41798bb95f
Ivy <chiongivydenise@gmail.com>
2025-09-17 19:34:14 +0800

    Implement Auth Form UI

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,10 +1,21 @@
+"use client";
+
 import React from "react";
+
+import AuthForm from "@/components/forms/AuthForm";
+import { SignInSchema } from "@/lib/validations";
 
 const SignIn = () => {
   return (
-    <div>
-      <p>SignIn</p>
-    </div>
+    <AuthForm
+      formType="SIGN_IN"
+      schema={SignInSchema}
+      defaultValues={{
+        email: "",
+        password: "",
+      }}
+      onSubmit={(data) => Promise.resolve({ success: true, data })}
+    />
   );
 };
 
---
+++ lib/validations.ts
@@ -0,0 +1,45 @@
+import z from "zod";
+
+export const SignInSchema = z.object({
+  email: z
+    .email("Please provide a valid email address")
+    .min(1, "Email is required"),
+
+  password: z
+    .string()
+    .min(6, "Password must be at least 6 characters long.")
+    .max(100, "Password cannot exceed 100 characters."),
+});
+
+export const SignUpSchema = z.object({
+  username: z
+    .string()
+    .min(3, "Username must be atleast 3 characters long.")
+    .max(30, "Username cannot exceed 30 characters.")
+    .regex(
+      /^[a-zA-Z0-9_]+$/,
+      "Username can only contain letters, numbers, and underscores."
+    ),
+
+  name: z
+    .string()
+    .min(1, "Name is required.")
+    .max(50, "Name cannot exceed 50 characters.")
+    .regex(/^[a-zA-Z\s]+$/, "Name can only contain letters and spaces."),
+
+  email: z
+    .email("Please provide a valid email address.")
+    .min(1, "Email is required."),
+
+  password: z
+    .string()
+    .min(6, "Password must be at least 6 characters long.")
+    .max(100, "Password cannot exceed 100 characters.")
+    .regex(/[A-Z]/, "Password must contain at least one uppercase letter.")
+    .regex(/[a-z]/, "Password must contain at least one lowercase letter.")
+    .regex(/[0-9]/, "Password must contain at least one number.")
+    .regex(
+      /[^a-zA-Z0-9]/,
+      "Password must contain at least one special character."
+    ),
+});
