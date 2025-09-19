8d78e98125e7b9a68af20e532325f8de979d5167
Ivy <chiongivydenise@gmail.com>
2025-09-19 22:34:34 +0800

    Implement Right Sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,29 +1,10 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
+import { auth } from "@/auth";
 
 const Home = async () => {
   const session = await auth();
   console.log(session);
 
-  return (
-    <div className="px-10 pt-[100px] space-x-2.5">
-      <h1 className="h1-bold">
-        {`Welcome to the world of Next.js ${session?.user?.name}!`}
-      </h1>
-      <form
-        action={async () => {
-          "use server";
-
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit" className="cursor-pointer">
-          Log out
-        </Button>
-      </form>
-    </div>
-  );
+  return <div className="px-10 pt-[100px] space-x-2.5"></div>;
 };
 
 export default Home;
