ab8570e4ad6a7776e387c65177ed730a85d1674b
Ivy <chiongivydenise@gmail.com>
2025-10-22 12:15:44 +0800

    Implement Create & Read User Routes

--- lib/validations.ts
+++ lib/validations.ts
@@ -62,3 +62,14 @@ export const AskQuestionSchema = z.object({
     .min(1, "At least one tag is required.")
     .max(3, "Cannot add more than 3 tags."),
 });
+
+export const UserSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  username: z.string().min(3, "Username must be at least 3 characters long."),
+  email: z.email("Please provide a valid email address."),
+  bio: z.string().optional(),
+  image: z.url("Please provide a valid URL.").optional(),
+  location: z.string().optional(),
+  portfolio: z.url("Please provide a valid URL").optional(),
+  reputation: z.number().optional(),
+});
